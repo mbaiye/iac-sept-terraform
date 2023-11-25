0d851ea159f46a65f50946646c62ad09564d0d7d
Moyosore Baiye <moyosorebaiye@gmail.com>
2023-11-25 20:14:49 +0100

    created vpc file

--- variables.tf
+++ variables.tf
@@ -14,3 +14,40 @@ variable "environment" {
   description = "Environment"
   type        = string
 }
+
+#vpc variables
+
+variable "vpc_cidr" {
+  description = "VPC cidr block"
+  type        = string
+}
+
+variable "public_subnet_cidr_az1" {
+  description = "Public Subnet AZ1 CIDR"
+  type        = string
+}
+
+variable "public_subnet_cidr_az2" {
+  description = "Public Subnet AZ2 CIDR"
+  type        = string
+}
+
+variable "private_app_subnet_cidr_az1" {
+  description = "Private App Subnet AZ1 CIDR"
+  type        = string
+}
+
+variable "private_app_subnet_cidr_az2" {
+  description = "Private App Subnet AZ2 CIDR"
+  type        = string
+}
+
+variable "private_data_subnet_cidr_az1" {
+  description = "Private Data Subnet AZ1 CIDR"
+  type        = string
+}
+
+variable "private_data_subnet_cidr_az2" {
+  description = "Private Data Subnet AZ2 CIDR"
+  type        = string
+}
